b5912932e982460c552ae36ebea386fa84b6afdb
jshemas <jshemas@hotmail.com>
2014-10-06 16:49:58 -0400

    build out some grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,110 @@
+module.exports = function (grunt) {
+	// Project configuration.
+	grunt.initConfig({
+		// lint all the js files
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			clientFiles: {
+				src: 'app/js/*.js'
+			}
+		},
+		// install bower stuff
+		bower: {
+			prod: {
+				options: {
+					targetDir: './dist/bower_components/'
+				}
+			},	
+			dev: {
+				options: {
+					targetDir: './app/bower_components/'
+				}
+			}
+		},
+		// move files from client and into dist
+		copy: {
+			dist: {
+				cwd: 'app',
+				src: [ '**' ],
+				dest: 'dist',
+				expand: true
+			},
+		},
+		// clean(deletes) the dist folder
+		clean: {
+			dist: {
+				src: ['dist']
+			},
+			styles: {
+				src: ['dist/css/*.css', '!dist/css/app.css']
+			},
+			scripts: {
+				src: ['dist/js/*.js', '!dist/js/app.js']
+			}
+		},
+		// minifies CSS
+		cssmin: {
+			dist: {
+				files: {
+					'dist/css/app.css': 'app/css/*.css'
+				}
+			}
+		},
+		// minifies JS
+		uglify: {
+			dist: {
+				options: {
+					mangle: false
+				},
+				files: {
+					'dist/js/app.js': 'app/js/*.js'
+				}
+			}
+		},
+		// http server
+		'http-server': {
+			'dev': {
+				// the server root directory
+				root: './app',
+				port: 8080,
+				host: "127.0.0.1",
+				// server default file extension
+				ext: "html",
+				// run in parallel with other tasks
+				runInBackground: false
+			},
+			'prod': {
+				// the server root directory
+				root: './dist',
+				port: 8080,
+				host: "127.0.0.1",
+				// server default file extension
+				ext: "html",
+				// run in parallel with other tasks
+				runInBackground: false
+			}
+		},
+		// live watcher for file changes
+		watch: {
+			clientFiles: {
+				files: ['app/*'],
+				tasks: ['jshint', 'default']
+			}
+		}
+	});
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-bower-task');
+	grunt.loadNpmTasks('grunt-http-server');
+	// Tasks.
+	grunt.registerTask('default', ['bower:dev', 'http-server:dev']);
+	grunt.registerTask('prod', ['bower:prod', 'build', 'http-server:prod']);
+	grunt.registerTask('build', ['clean:dist', 'copy', 'cssmin', 'uglify', 'clean:styles', 'clean:scripts']);
+};
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 reallyawesomedevelopers.github.io
 ==================================
 Under Construction
+
+To run dev locally:
+```npm install && grunt``
+If you want to run prod locally:
+```npm install && grunt prod```
+Then go to:
+```http://localhost:8080/```
